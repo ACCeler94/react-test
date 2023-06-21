31e198d52ab8973d29d4513f9fb21e7be4019b3b
Artur C <araghast94@icloud.com>
2023-06-21 16:25:31 +0200

    Add redux store support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
